271b4d39866dca2f863befd348e90b23b4cf7322
SungJin-Jin <deskhi@nethru.co.kr>
2018-02-14 13:10:56 +0900

    Remove toast

--- app/src/main/java/io/datalive/deeplink/extension/ContextExtension.kt
+++ app/src/main/java/io/datalive/deeplink/extension/ContextExtension.kt
@@ -2,6 +2,7 @@ package io.datalive.deeplink.extension
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
@@ -17,6 +18,16 @@ fun Context.loggingQueryParams(intent: Intent, key: String) = {
     }
 
     logging(key, bundle)
+}
+
+fun Context.moveActivityWithExtraData(destIntent: Intent, receiverIntent: Intent) {
+    destIntent.data = Uri.parse("http://nethrureferrer-4b919.firebaseapp.com?" +
+            "${receiverIntent.extras.get("referrer")}"
+    )
+
+    destIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+    startActivity(destIntent)
 }
 
 fun Context.toast(message:String) = Toast.makeText(this, message, Toast.LENGTH_LONG).show()
--- app/src/main/java/io/datalive/deeplink/receiver/ReferrerReceiver.kt
+++ app/src/main/java/io/datalive/deeplink/receiver/ReferrerReceiver.kt
@@ -3,27 +3,22 @@ package io.datalive.deeplink.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import io.datalive.deeplink.activity.AfterInstallActivity
 import io.datalive.deeplink.activity.DeepLinkActivity
 import io.datalive.deeplink.activity.DynamicLinkActivity
-import io.datalive.deeplink.extension.toast
+import io.datalive.deeplink.extension.moveActivityWithExtraData
 
 class ReferrerReceiver : BroadcastReceiver() {
 
     companion object {
-        private val ACTION_INSTALL_REFERRER = "com.android.vending.INSTALL_REFERRER"
+        private const val ACTION_INSTALL_REFERRER = "com.android.vending.INSTALL_REFERRER"
     }
 
     override fun onReceive(context: Context, receiverIntent: Intent) {
         when (receiverIntent.action) {
             ACTION_INSTALL_REFERRER -> {
                 with(receiverIntent.extras) {
-                    context.toast(keySet().joinToString { key -> getString(key) })
-
-                    var paramMap = toMap(getString("referrer"))
-
-//                    context.toast(paramMap.toString())
+                    val paramMap = toMap(getString("referrer"))
 
                     val destIntent = when (paramMap["where"]) {
                         "deepLink" -> Intent(context, DeepLinkActivity::class.java)
@@ -31,28 +26,16 @@ class ReferrerReceiver : BroadcastReceiver() {
                         else -> Intent(context, AfterInstallActivity::class.java)
                     }
 
-                    moveActivity(destIntent, receiverIntent, context)
+                    context.moveActivityWithExtraData(destIntent, receiverIntent)
                 }
             }
         }
     }
 
-    private fun toMap(queryString: String): Map<String, String> {
-        return queryString.split("&").map {
-            it.split("=")
-        }.map { it[0] to it[1] }.toMap()
-    }
+    private fun toMap(queryString: String) =
+            queryString.split("&").map {
+                it.split("=")
+            }.map { it[0] to it[1] }.toMap()
 
-    private fun moveActivity(destIntent: Intent, receiverIntent: Intent, context: Context) {
-        with(destIntent) {
-            destIntent.data = Uri.parse("http://nethrureferrer-4b919.firebaseapp.com?" +
-                    "${receiverIntent.extras.get("referrer")}"
-            )
-
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-            context.startActivity(this)
-        }
-    }
 }
 
