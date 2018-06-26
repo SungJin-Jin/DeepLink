34bcf629d3151659bc3edceec81fbb413e213ba8
SungJin-Jin <deskhi@nethru.co.kr>
2018-06-26 23:16:22 +0900

    Fix onCreate at BaseActivity

--- app/src/main/java/io/datalive/deeplink/common/BaseActivity.kt
+++ app/src/main/java/io/datalive/deeplink/common/BaseActivity.kt
@@ -1,14 +1,13 @@
 package io.datalive.deeplink.common
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
 import io.datalive.deeplink.log.Logger
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         Logger.logEvent(this, intent.data, getAction())
     }
